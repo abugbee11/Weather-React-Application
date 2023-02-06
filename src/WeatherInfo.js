bb15b9f0a17461fb8717eea16d1f5317bd882b07
Adrienne B <abugbee11@gmail.com>
2023-02-05 19:37:58 -0500

    Updated main display icon to react animated weather  JS component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import react from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherForecast from "./WeatherForecast";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,11 +18,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-md-6">
           <div className="clearfix weather-temperature">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="current-image"
-            />
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
             <strong>{Math.round(props.data.temperature)}</strong>
             <span className="units">
               <a href="/" className="active">
